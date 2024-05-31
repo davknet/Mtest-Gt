5a6f05b7ade28e5ed393e3dea074d5290a5b0b83
david kahadze <davknet1@gmail.com>
2024-05-31 18:36:02 +0300

    the plugin for testing has been established

---
+++ mtest-gt.php
@@ -0,0 +1,20 @@
+<?php 
+
+/*
+ * Plugin Name:       MTest-Gt
+ * Plugin URI:        https://example.com/plugins/the-basics/
+ * Description:        the plugin developed for testing purppeses 
+ * Version:           1.0.0.0
+ * Requires at least: 5.5
+ * Requires PHP:      7.2
+ * Author:           David Kahadze 
+ * Author URI:        https://author.example.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:        mtest-gt 
+ * Domain Path:       /languages
+ */ 
+
+
+ 
